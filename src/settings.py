eb801df1448f38269abe2c791a42d1500018dfdb
girishsontakke <girishsontakke7@gmail.com>
2021-03-02 16:34:01 +0530

    :rocket: deployment commands for heroku

--- src/settings.py
+++ src/settings.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 from decouple import config
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -129,3 +130,6 @@ SWAGGER_SETTINGS = {
         }
     },
 }
+
+# Activate Django-Heroku.
+django_heroku.settings(locals())
