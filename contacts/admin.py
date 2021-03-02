aa3ca80aed98593eaa88660920dfda50a3708f35
girishsontakke <girishsontakke7@gmail.com>
2021-03-02 20:17:17 +0530

    :sparkles: register contacts in admin page

--- contacts/admin.py
+++ contacts/admin.py
@@ -1,3 +1,12 @@
+from .models import Contacts
 from django.contrib import admin
 
 # Register your models here.
+
+
+@admin.register(Contacts)
+class ContactsAdmin(admin.ModelAdmin):
+    list_display = ["firstname", "lastname", "phone_number", "owner"]
+    search_fields = ["firstname", "lastname", "phone_number"]
+    list_filter = ["firstname", "lastname", "owner", "phone_number"]
+    list_per_page = 25
